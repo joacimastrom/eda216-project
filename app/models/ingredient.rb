b1b8692b25be1f4530ffececdd22e5ba7e182715
joacim <joacim.astrom@gmail.com>
2016-03-31 00:07:49 +0200

    whatevs

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,5 @@ class Ingredient < ActiveRecord::Base
 	has_many :recipes
 
 	validates_presence_of :name, :amount_delivered, :quantity
-	validates_numericality_of :amount_delivered, greater_than: 0 
 
 end
